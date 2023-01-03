0e4db2cfc59af4d09bc7ab138d1d64a2f87a09ab
mani-2012 <106404786+mani-2012@users.noreply.github.com>
2023-01-02 21:42:06 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,24 @@
 function setup() {
-    canvas = createCanvas(300, 300);
-    canvas.position(517,300);
-    video = createCapture(VIDEO);
-    video.hide();
-  }
-  function draw(){
-    image(video,0,0,300,300);
-  }
+  cuadro = createCanvas(300, 300);
+  cuadro.position(515,250);
+  video = createCapture(VIDEO);
+  video.hide();
+  plumones=ml5.imageClassifier('MobileNet',reloj);
+}
+function reloj(){
+  console.log(' YA ESTA LISTO TU MODELO PARA OCUPAR :) ');
+}
+function draw(){
+  image(video,0,0,300,300);
+  plumones.classify(video,vaso)
+}
+function vaso(error,respuesta){
+if(error){
+  console.error(error);
+}
+else{
+  console.log(respuesta);
+  document.getElementById("perla").innerHTML=respuesta[0].label;
+  document.getElementById("name").innerHTML=respuesta[0].confidence.toFixed(2);
+}
+}
